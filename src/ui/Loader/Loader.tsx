fb792d49a2f18fd09c5de699cdbc6ce4c60d7fc3
Igor Storchevoy <hellsgor@gmail.com>
2024-08-23 23:44:45 +0400

    Add: create Loader

---
+++ src/ui/Loader/Loader.tsx
@@ -0,0 +1,15 @@
+import './Loader.css';
+
+export const Loader = () => (
+  <div className="loader">
+    <div className="container">
+      <div className="loader__inner">
+        <div className="loader__wrapper">
+          <div className="loader-item"></div>
+          <div className="loader-item"></div>
+          <div className="loader-item"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+);

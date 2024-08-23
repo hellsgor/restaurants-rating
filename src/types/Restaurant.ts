7f184d3d76856b65adf05cad3e8af1a5fcf4743d
Igor Storchevoy <hellsgor@gmail.com>
2024-08-23 23:02:38 +0400

    Update: updated types

---
+++ src/types/Restaurant.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const RestaurantSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  type: z.string(),
+  rating: z.number(),
+  url: z.string(),
+});
+
+export type Restaurant = z.infer<typeof RestaurantSchema>;
+
+export interface UpdateRestaurantRatingArgs {
+  id: Restaurant['id'];
+  rating: Restaurant['rating'];
+}

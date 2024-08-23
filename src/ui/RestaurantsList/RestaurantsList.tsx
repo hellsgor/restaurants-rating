bf5f360dadf83ac8d67d2e21d684337c7d1ce0bd
Igor Storchevoy <hellsgor@gmail.com>
2024-08-24 02:40:50 +0400

    Update: displayed restaurant cards

--- src/ui/RestaurantsList/RestaurantsList.tsx
+++ src/ui/RestaurantsList/RestaurantsList.tsx
@@ -1,61 +1,28 @@
 import './RestaurantsList.css';
 
 import { RestaurantCard } from '../RestaurantCard/RestaurantCard';
+import { useContext } from 'react';
+import { RestaurantsContext } from '../../contexts/RestaurantsContext';
 
 export const RestaurantsList = () => {
+  const restaurants = useContext(RestaurantsContext).data;
+
   return (
     <section className="restaurants-list">
       <div className="container">
         <ul className="restaurants-list__items">
-          <li className="restaurants-list__item">
-            <RestaurantCard
-              id="1"
-              name="Mama's Kitchen"
-              type="American"
-              rating={4.7}
-              url="/mamas-kitchen.png"
-            />
-          </li>
-
-          <li className="restaurants-list__item">
-            <RestaurantCard
-              id="2"
-              name="The Burger Joint"
-              type="American"
-              rating={4.6}
-              url="/burger-joint.png"
-            />
-          </li>
-
-          <li className="restaurants-list__item">
-            <RestaurantCard
-              id="3"
-              name="Pasta Express"
-              type="American"
-              rating={3.4}
-              url="/pasta-express.png"
-            />
-          </li>
-
-          <li className="restaurants-list__item">
-            <RestaurantCard
-              id="4"
-              name="Taco Fiesta"
-              type="American"
-              rating={4.8}
-              url="/taco-fiesta.png"
-            />
-          </li>
-
-          <li className="restaurants-list__item">
-            <RestaurantCard
-              id="4"
-              name="Taco Fiesta"
-              type="American"
-              rating={4.8}
-              url="/taco-fiesta.png"
-            />
-          </li>
+          {restaurants &&
+            restaurants.map((item) => (
+              <li className="restaurants-list__item" key={item.id}>
+                <RestaurantCard
+                  id={item.id}
+                  name={item.name}
+                  type={item.type}
+                  rating={item.rating}
+                  url={item.url}
+                />
+              </li>
+            ))}
         </ul>
       </div>
     </section>

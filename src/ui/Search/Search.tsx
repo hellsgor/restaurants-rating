1f67b8118c3cb392f708b4221ad06678e2a6fe0d
Igor Storchevoy <hellsgor@gmail.com>
2024-08-25 19:20:50 +0400

    Update: updated Search

--- src/ui/Search/Search.tsx
+++ src/ui/Search/Search.tsx
@@ -1,11 +1,27 @@
+import React, { FC } from 'react';
 import './Search.css';
 
-export const Search = () => {
+type SearchProps = {
+  value: string | undefined;
+  placeholder?: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const Search: FC<SearchProps> = ({
+  value,
+  onChange,
+  placeholder = null,
+}) => {
   return (
     <section className="search">
       <div className="container">
         <div className="search__inner">
-          <input type="text" placeholder="Search for restaurants" />
+          <input
+            type="text"
+            placeholder={placeholder ? placeholder : 'Enter the search query'}
+            onChange={onChange}
+            value={value}
+          />
         </div>
       </div>
     </section>
